380ed53d5298c2e58ed307f2b18b8fb21122046b
patztablook22 <patz@tuta.io>
2021-10-13 22:10:26 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir build
-cmake -S . -B build && cd build && make
+cmake -S . -B build -DCMAKE_BUILD_TYPE=Release && cd build && make
 echo 'executable successfully built in `build`'
